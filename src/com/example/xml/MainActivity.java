f785b2d0e51721389ce74588d373195873f0b9a8
kabir <jxwazxzhp@126.com>
2016-07-20 21:19:15 +0800

    init.

---
+++ src/com/example/xml/MainActivity.java
@@ -0,0 +1,70 @@
+package com.example.xml;
+
+import java.util.Map;
+
+import android.R.bool;
+import android.app.Activity;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+	private static final String tag = MainActivity.class.getName();
+	private EditText et_name;
+	private EditText et_passwd;
+	private CheckBox cb_ischeck;
+	private UserInfoUtils userInfoUtils;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		et_name = (EditText) findViewById(R.id.et_username);
+		et_passwd = (EditText) findViewById(R.id.et_password);
+		cb_ischeck = (CheckBox) findViewById(R.id.cb_ischeck);
+		userInfoUtils = new UserInfoUtils(this);
+		Map<String, String> maps = userInfoUtils.readInfo();
+		if(maps != null){
+			String name = maps.get("name");
+			String pwd = maps.get("pwd");
+			if(TextUtils.isEmpty(name) || TextUtils.isEmpty(pwd)){
+			}else{
+				et_name.setText(name);
+				et_passwd.setText(pwd);
+				cb_ischeck.setChecked(true);
+			}
+		}
+	}
+
+	public void onClick(View v) {
+		String name = et_name.getText().toString().trim();
+		String pwd = et_passwd.getText().toString().trim();
+
+		if (TextUtils.isEmpty(name) || TextUtils.isEmpty(pwd)) {
+			Toast.makeText(this, "用户名和密码不能为空", Toast.LENGTH_SHORT).show();
+		} else {
+			if (cb_ischeck.isChecked()) {
+				boolean result = userInfoUtils.saveInfo(name, pwd);
+				if (result) {
+
+					Toast.makeText(this, "保存成功", Toast.LENGTH_SHORT).show();
+				} else {
+					Toast.makeText(this, "保存失败", Toast.LENGTH_SHORT).show();
+
+				}
+			} else {
+				Toast.makeText(this, "请选择保存用户名和密码", Toast.LENGTH_SHORT).show();
+
+			}
+		}
+	}
+
+}
---
+++ src/com/example/xml/UserInfoUtils.java
@@ -0,0 +1,56 @@
+package com.example.xml;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+import android.content.Context;
+
+public class UserInfoUtils {
+	private File file;
+
+	public UserInfoUtils(Context context) {
+		super();
+		file = new File(context.getFilesDir().getPath(), "info.txt");
+	}
+
+	public boolean saveInfo(String name, String pwd) {
+		String result = name + "##" + pwd;
+		try {
+			FileOutputStream fos = new FileOutputStream(file);
+			fos.write(result.getBytes());
+			fos.close();
+			return true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+
+	public Map<String, String> readInfo() {
+		try {
+			Map<String, String> maps = new HashMap<String, String>();
+			FileInputStream fis = new FileInputStream(file);
+			BufferedReader bufr = new BufferedReader(new InputStreamReader(fis));
+			String content = bufr.readLine();
+			System.out.println(content);
+			String[] splits = content.split("##");
+			String name = splits[0];
+			String pwd = splits[1];
+			maps.put("name", name);
+			maps.put("pwd", pwd);
+			return maps;
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return null;
+
+	}
+}
